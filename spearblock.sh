d8c54e88c461d3dfb0439856959e00540f0f35db
Ahmad Nazari <ahmad.nazari73@yahoo.com>
2023-08-15 18:34:05 +0330

    Update spearblock.sh

--- spearblock.sh
+++ spearblock.sh
@@ -714,11 +714,11 @@ show_headers(){
 
   #header
   separator
-  echo -e "\e[0;30;47m                                                                \e[0m"
-  echo -e "\e[0;30;47m                  [ SPEAR MASTER SCRIPT ]                       \e[0m"
-  echo -e "\e[0;30;47m                      Author: spearvpn                          \e[0m"
-  echo -e "\e[0;30;47m                Telegram Channel: @spearvpn                     \e[0m"
-  echo -e "\e[0;30;47m                                                                \e[0m"
+  echo -e "\e[0;30;43m                                                                \e[0m"
+  echo -e "\e[0;30;43m                  [ SPEAR MASTER SCRIPT ]                       \e[0m"
+  echo -e "\e[0;30;43m                      Author: spearvpn                          \e[0m"
+  echo -e "\e[0;30;43m                Telegram Channel: @spearvpn                     \e[0m"
+  echo -e "\e[0;30;43m                                                                \e[0m"
   separator
 }
 #----------------------------------------------------------------------------------------------------------------------- menu functions
